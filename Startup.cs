09d7e2e05f67648b94707018118e7a43d387435a
mgoshen1 <42220451+mgoshen1@users.noreply.github.com>
2018-08-13 10:56:05 -0700

    Changes to add instrmentation

--- Startup.cs
+++ Startup.cs
@@ -25,10 +25,11 @@ namespace Module2Lab1
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+      public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
+            services.AddApplicationInsightsTelemetry(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,5 +57,21 @@ namespace Module2Lab1
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        // This was added according to EDS specifications.
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+        }  
     }
 }
